6b8d0d07b7e95e462c250a2858e3cc8f9e7959b0
dongsuk.chun <ikarus1004@gmail.com>
2022-04-02 14:42:50 +0900

    implement instagram first page

--- dj4_instagram/settings.py
+++ dj4_instagram/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'content.apps.ContentConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'dj4_instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
